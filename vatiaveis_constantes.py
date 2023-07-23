6e261e9b30e25fe426b72f9730493bc5c3aa0b82
Rafael Ribeiro <rafa210488@gmail.com>
2023-07-23 11:28:47 -0300

    Boas práticas, variáveis e constantes em python

---
+++ vatiaveis_constantes.py
@@ -0,0 +1,23 @@
+# Variáveis
+nome = 'Rafael'
+idade = 35
+
+print(f'{nome} tem {idade} anos de idade.')
+# Alterando o valor de uma variável
+
+nome = 'Karina'
+idade = 32
+
+print(f'{nome} tem {idade} anos de idade.')
+
+# atribuindo múltiplas variáveis em uma linha
+valor, peso = (25, 85)
+print(f'valor: {valor}\npeso: {peso}')
+
+
+# snake case e nomes sugestivos de variáveis
+limite_saque_diario = 1000
+
+# Constante em letras maiúsculas
+ESTADOS = ['PR', 'SP', 'RJ']
+
