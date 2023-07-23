e661c7f30b757fb3cb984d0a07f9e6679606623d
Rafael Ribeiro <rafa210488@gmail.com>
2023-07-22 22:45:16 -0300

    tipos de dados em Python.

---
+++ tipos-de-dados.py
@@ -0,0 +1,12 @@
+"""TIPOS DE DADOS EM PYTHON
+
+Tipos built-in:
+
+- Texto - str
+- Numérico - int, float, complex
+- Sequência - list, tuple, range
+- Mapa - dict
+- Coleção - set, frozenset
+- Booleano - bool
+    - Qualquer número diferente de 0 representa verdadeiro, e 0 representa falso.
+- Binário = bytes, bytearray, memoryview"""
