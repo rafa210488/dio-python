9b972313f8417afb0281c9c5f22defefd59ee8f5
Rafael Ribeiro <rafa210488@gmail.com>
2023-07-23 15:04:07 -0300

    conversão de tipos, funções de entrada e saída.

---
+++ convertendo_tipos.py
@@ -0,0 +1,19 @@
+# Convertendo int para str
+numero = 10
+print(type(numero))
+numero = str(numero)
+print(type(numero))
+
+# Convertendo int para float
+numero = float(numero)
+print(numero)
+print(type(numero))
+
+# Convertendo de str para int ou float
+valor_str = '25.2'
+valor_str2 = '25'
+valor_int = int(valor_str2)
+valor_float = float(valor_str)
+print(valor_str, type(valor_str))
+print(valor_int, type(valor_int))
+print(valor_float, type(valor_float))
---
+++ print_input.py
@@ -0,0 +1,6 @@
+# Recebendo dados do usuário
+nome = input('Informe seu nome: ')
+idade = int(input('Qual a sua idade: '))
+# Mostrando dados do usuário
+print(nome, idade, end=' ...\n')
+print(f'{nome} tem {idade} anos')
