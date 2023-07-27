75cba217c16849ef9ed9a86e9a9a0110a7518e42
Rafael Ribeiro <rafa210488@gmail.com>
2023-07-26 22:57:40 -0300

    Strings: métodos e fatiamento

---
+++ string_metodos.py
@@ -0,0 +1,17 @@
+nome = 'rAFaEl'
+
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+texto = '  Olá, Mundo    '
+
+print(texto)
+print(texto.strip() + '.')
+print(texto.lstrip() + '.')
+print(texto.rstrip() + '.')
+
+
+menu = 'Python'
+print(menu.center(20, '#'))
+print('-'.join(menu))
---
+++ strings_fatiamento.py
@@ -0,0 +1,12 @@
+nome = 'Rafael Ribeiro dos Santos'
+
+# sintaxe [start:stop:step]
+
+print(nome[0])
+print(nome[-1])
+print(nome[:9])
+print(nome[10:])
+print(nome[10:16])
+print(nome[10:16:2])
+print(nome[:])
+print(nome[::-1])
